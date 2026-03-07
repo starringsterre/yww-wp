9190c5818576c948c3c1b117f77ffac34e7c9862
Sterre Molendijk <sterremolendijk@Sterres-MacBook-Air.local>
2026-03-07 14:45:31 +0100

    debug: catch and expose server init error

--- api/[...all].ts
+++ api/[...all].ts
@@ -1,3 +1,18 @@
-import { createServer } from "../server";
+let app: any;
+try {
+  const { createServer } = require("../server");
+  app = createServer();
+} catch (err: any) {
+  // If server import fails, return the error for debugging
+  app = (_req: any, res: any) => {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({
+      error: "Server initialization failed",
+      message: err?.message,
+      stack: err?.stack?.split("\n").slice(0, 5),
+    }));
+  };
+}
 
-export default createServer();
+export default app;

a408e72a819a6d33ab1a6ee066071e928d9e9cf4
Sterre Molendijk <sterremolendijk@Sterres-MacBook-Air.local>
2026-03-07 14:43:08 +0100

    debug: test express without serverless-http

--- api/test.ts
+++ api/test.ts
@@ -1,9 +1,8 @@
-import serverless from "serverless-http";
 import express from "express";
 
 const app = express();
 app.get("/api/test", (_req, res) => {
-  res.json({ ok: true, express: true });
+  res.json({ ok: true, express: true, noServerlessHttp: true });
 });
 
-export default serverless(app);
+export default app;

06f2122262f53dde3afd18562e5db7bbb6acc0d8
Sterre Molendijk <sterremolendijk@Sterres-MacBook-Air.local>
2026-03-07 14:46:45 +0100

    debug: dynamic import to capture actual server init error

--- api/[...all].ts
+++ api/[...all].ts
@@ -1,18 +1,15 @@
-let app: any;
-try {
-  const { createServer } = require("../server");
-  app = createServer();
-} catch (err: any) {
-  // If server import fails, return the error for debugging
-  app = (_req: any, res: any) => {
+export default async function handler(req: any, res: any) {
+  try {
+    const { createServer } = await import("../server");
+    const app = createServer();
+    return app(req, res);
+  } catch (err: any) {
     res.statusCode = 500;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({
       error: "Server initialization failed",
       message: err?.message,
-      stack: err?.stack?.split("\n").slice(0, 5),
+      stack: err?.stack?.split("\n").slice(0, 8),
     }));
-  };
+  }
 }
-
-export default app;
